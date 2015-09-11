f6937a3c14298fcf2d2240b7df0ba89d8c2f19ac
Joseph Dykstra <josephdykstra@gmail.com>
2015-09-11 10:49:44 -0500

    Fix style

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var divisors = {
 module.exports = function bitrate(bytes, seconds, format) {
 	if (format === undefined || format === null) format = 'kbps'
 	if (typeof format !== 'string') throw new TypeError('Expected \'format\' to be a string')
-	var divisor = divisors[format.replace('/', 'p')]
+	var divisor = divisors[ format.replace('/', 'p') ]
 	if (!divisor) throw new Error('\'format\' is an invalid string')
-	return bytes/seconds/divisor
+	return bytes / seconds / divisor
 }
